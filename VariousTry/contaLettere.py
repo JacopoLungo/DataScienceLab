4ad42d408211ce2dc7b6dbfeceedb835b011eb54
Jacopo Lungo Vaschetti <jacopo.lungo@hotmail.it>
2022-10-03 18:21:41 +0200

    no modifiche

--- VariousTry/contaLettere.py
+++ VariousTry/contaLettere.py
@@ -1,3 +1,5 @@
+import numpy
+import pandas as pd
 frase = input("inserisci la frase di cui vuoi contare i caratteri (spazi inclusi): ")
 tot=len(frase)
 print(f"Ci sono {tot} lettere nella frase: \"{frase}\"")
